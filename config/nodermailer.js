2a3f1dd10dcd1d2f4e216d7de2b2c84076c5e7d6
AdrianFred <adrian989@me.com>
2023-04-10 21:41:33 +0200

    Removing comments in config nodemailer

--- config/nodermailer.js
+++ config/nodermailer.js
@@ -1,13 +1,5 @@
 import nodemailer from "nodemailer";
 
-// export const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD,
-//   },
-// });
-
 export const transporter = nodemailer.createTransport({
   host: "smtp-auth.mailprotect.be",
   port: 587,
